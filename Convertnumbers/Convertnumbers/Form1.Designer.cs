297ab92edf2371e950a45de941bbc83c7bf6cdb8
unknown <furcis.burcis@gmail.com>
2024-08-02 12:16:02 +0200

    Changed some stuff

--- Convertnumbers/Convertnumbers/Form1.Designer.cs
+++ Convertnumbers/Convertnumbers/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,7 +50,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(146, 13);
+            this.label2.Location = new System.Drawing.Point(13, 58);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 16);
             this.label2.TabIndex = 1;
@@ -58,7 +59,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(325, 13);
+            this.label3.Location = new System.Drawing.Point(13, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 16);
             this.label3.TabIndex = 2;
@@ -73,32 +74,42 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(149, 33);
+            this.textBox2.Location = new System.Drawing.Point(13, 77);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(138, 22);
+            this.textBox2.Size = new System.Drawing.Size(190, 22);
             this.textBox2.TabIndex = 4;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(328, 33);
+            this.textBox3.Location = new System.Drawing.Point(13, 124);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 22);
             this.textBox3.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(174, 77);
+            this.button1.Location = new System.Drawing.Point(12, 179);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
             this.button1.Text = "Convert";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 149);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(469, 112);
+            this.ClientSize = new System.Drawing.Size(222, 214);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
@@ -108,6 +119,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +134,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Convertnumbers/Convertnumbers/Form1.cs
+++ Convertnumbers/Convertnumbers/Form1.cs
@@ -16,5 +16,29 @@ namespace Convertnumbers
         {
             InitializeComponent();
         }
+
+        private void CheckNumber(string number)
+        {
+            textBox2.Text = "";
+            textBox3.Text = "";
+            label4.Text = "";
+            float parse = 0;
+            if(!float.TryParse(number, out parse))
+            {
+                label4.Text = "Use only numbers!";
+            } else {
+                textBox2.Text = number;
+                textBox3.Text = number;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckNumber(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "Convertor";
+        }
     }
 }
